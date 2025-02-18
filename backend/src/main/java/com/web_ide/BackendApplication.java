e63e4d254d6904d87bd82b294bf5f31be21c3d4d
kichan kim <kkc7130coding@gmail.com>
2025-02-18 14:41:19 +0900

    backend first

---
+++ backend/src/main/java/com/web_ide/BackendApplication.java
@@ -0,0 +1,23 @@
+package com.web_ide;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+	
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
+}
