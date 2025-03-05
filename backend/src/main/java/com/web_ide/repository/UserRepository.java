9bca16432b4152afe09f63e4e68f423202e096b4
Jongrok98 <qkrwhdfhr98@naver.com>
2025-03-05 18:07:52 +0900

    Update UserRepository.java

--- backend/src/main/java/com/web_ide/repository/UserRepository.java
+++ backend/src/main/java/com/web_ide/repository/UserRepository.java
@@ -4,8 +4,6 @@ import com.web_ide.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByLoginId(String loginId);
     boolean existsByEmail(String email);
